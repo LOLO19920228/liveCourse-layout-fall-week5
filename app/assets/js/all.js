d1045b8a4d37c21b805513fda51edfee70d78919
Tim Chou <lolo19920228@gmail.com>
2020-11-17 11:26:05 +0800

    增加ALL(4)的JQ

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,8 @@
 $(document).ready(() => {
   console.log('Hello Bootstrap4');
+  $("#allAssignmentBtn").click(function (e) {
+    e.preventDefault();
+    $(this).siblings('#list-tab').slideToggle();
+    console.log('Hello Bootstrap666666');
+});
 });
